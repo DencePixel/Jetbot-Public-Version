4b931a65c9abfca44059bbfe0a14460b16da1435
DencePixel <104887467+DencePixel@users.noreply.github.com>
2023-02-27 17:22:05 +0000

    Update main.py

--- Jetbot Github Version/main.py
+++ Jetbot Github Version/main.py
@@ -6,12 +6,13 @@ import json
 import platform
 from configuration import *
 import logging
+from dislash import InteractionClient, ActionRow, Button, ButtonStyle
 
 class Client(commands.Bot):
     def __init__(self):
         super().__init__(command_prefix=commands.when_mentioned_or(bot_prefix), intents=discord.Intents().all())
 
-        self.cogslist = ["cogs.funcogs.rizzlevel","cogs.funcogs.gayrate","cogs.admincogs.kick","cogs.admincogs.ban","cogs.maincogs.rosearch","cogs.maincogs.ping","cogs.botlogs.rolelog","cogs.botlogs.channellog","cogs.botlogs.memberlog","cogs.maincogs.welcome","cogs.admincogs.announce","cogs.admincogs.handler"]
+        self.cogslist = ["cogs.funcogs.rizzlevel","cogs.funcogs.gayrate","cogs.admincogs.kick","cogs.admincogs.ban","cogs.maincogs.rosearch","cogs.maincogs.ping","cogs.botlogs.rolelog","cogs.botlogs.channellog","cogs.botlogs.memberlog","cogs.maincogs.welcome","cogs.admincogs.announce","cogs.admincogs.handler","cogs.botlogs.messagelog","cogs.admincogs.antilink","cogs.admincogs.slowmode","cogs.maincogs.repo","cogs.admincogs.purge"]
 
     async def setup_hook(self):
       for ext in self.cogslist:
@@ -25,6 +26,11 @@ class Client(commands.Bot):
         print(prfx + " Python Version " + Fore.YELLOW + str(platform.python_version()))
         synced = await self.tree.sync()
 
+
+    
+
 client = Client()
+
+
 
 client.run(token, log_handler=handler)
